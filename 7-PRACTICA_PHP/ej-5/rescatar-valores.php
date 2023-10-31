cb5cdaf02dae1e338c91ab16614331856a23ab6e
Camila <camilasalvia2@gmail.com>
2023-10-31 20:24:00 -0300

    update

---
+++ 7-PRACTICA_PHP/ej-5/rescatar-valores.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2>El nombre es</h2>
+  <p><?php echo $_SESSION["nombre"] ?></p>
+  <h2>La clave es</h2>
+  <p><?php echo $_SESSION["clave"] ?></p>
+</body>
+</html>
--- 7-PRACTICA_PHP/ej-5/sesion.php
+++ 7-PRACTICA_PHP/ej-5/sesion.php
@@ -5,14 +5,3 @@
   $_SESSION["clave"]=$_POST["clave"];
   }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <h3>Nombre</h3>
-</body>
-</html>
