4d25b4f004ca43c3cd58e3a2ba3f80161d45fe92
Camila <camilasalvia2@gmail.com>
2023-10-31 21:42:39 -0300

    hacer

--- 7-PRACTICA_PHP/ej-8/conexion.php
+++ 7-PRACTICA_PHP/ej-8/conexion.php
@@ -1,4 +1,4 @@
 <?php
-$connection = mysqli_connect("localhost", "root");
-mysqli_select_db("prueba",$connection);
+$connection = mysqli_connect("localhost", "root", DB_PASS, "prueba");
+mysqli_select_db($connection, "prueba");
 ?>
